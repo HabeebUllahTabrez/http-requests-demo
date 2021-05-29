7bb960a0a988917f825978340dbce88a42fcf835
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2021-05-29 15:03:39 +0530

    Initial Commit

---
+++ my-svelte-project/src/components/login/Login.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { Button, TextField } from "smelte";
+</script>
+
+<TextField label="Email Address" outlined />
+<TextField label="Password" outlined />
+<Button dark>Button</Button>
